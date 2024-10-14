ffa9ab14addd4791dbc5f91aed7779ac4750a8e0
Haider Shah <stfox302@gmail.com>
2024-10-14 15:59:48 +0500

    Directory Structure Done -> Start Codeing the authentication systems

--- app.vue
+++ app.vue
@@ -1,16 +1,9 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-      <div class="artboard phone-1 shadow-xl shadow-primary/30 rounded-box mx-auto p-4">
-          <h1 class="text-2xl font-mono mb-3">
-              Welcome For Fly Live - testing if Installed modules are working fine
-          </h1>
-          <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto cum exercitationem facere facilis, quia quis quod similique tempora ullam. Reiciendis!
-          </p>
-      </div>
-
+      <NuxtRouteAnnouncer />
+      <NuxtLoadingIndicator />
+      <NuxtLayout>
+          <NuxtPage />
+      </NuxtLayout>
   </div>
 </template>
-<script setup lang="ts">
-</script>
